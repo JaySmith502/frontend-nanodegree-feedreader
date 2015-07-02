09a4e29728ce8b6003a057b5e7210c2dbb582fad
smith1jason <smith1jason@users.noreply.github.com>
2015-07-02 18:35:18 -0400

    Remove TODO's

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,10 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         **space
+        /* 
          **Used allFeeds.forEach to iterate through each instance of the feeds
          **toBeDefined worked as needed here, and used not.toBe(0) to ensure something there as needed
          */
@@ -42,11 +39,6 @@ $(function() {
             });
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         //**do this the same as the URL test above, works as above
         it('have a name', function() {
             allFeeds.forEach(function(feed) {
@@ -56,28 +48,16 @@ $(function() {
         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
     describe('The Menu', function() {
         //assign local variable to menuVisibility and menuClicker for global use within this suite
         var menuVisibility = $('body').attr("class");
         var menuClicker = $(".menu-icon-link");
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         * separate TODO from my comments
-         **check for the css class 'menu-hidden' which tells us this element is not visible
-         */
+        //check for the css class 'menu-hidden' which tells us this element is not visible
+        
         it('is hidden by default', function() {
             expect(menuVisibility).toBe('menu-hidden');
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
-         */
         //**clicks on the menu to open and close it to check it's inheritance of 'menu-hidden' or lack thereof
         it('toggles visibility', function() {
 
@@ -90,36 +70,23 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries"*/
     describe('Initial Entries', function() {
-        /* use beforEach to call the feeds before the test runs (SETUP)
+        /* use beforeEach to call the feeds before the test runs (SETUP)
          * this will load up the first feed in the array, which will allow 'it' to check it against our criteria
          */
         beforeEach(function(done) {
 
             loadFeed(0, done);
         });
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         * space
-         **after the beforeEach function loads the feed, below checks to see if it has length>0, which indicates something is there
-         */
+        //after the beforeEach function loads the feed, below checks to see if it has length>0, which indicates something is there
         it('show at least a single entry at completion of call', function(done) {
 
             expect($(".entry").length).not.toBe(0);
             done();
         });
     });
-    /* TODO: Write a new test suite named "New Feed Selection"*/
 
     describe('New Feed Selection', function() {
-        /** TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.*/
-
         //used Math.random so test could iterate through any feed choice on the current menu, for future iterations a counter could be used to address the addition of more feeds or just hard code the end number
         //according to Udacity reviewer my use of Math.random is buggy because it only returns between 0 and 1, so I'm going to scrap that and go with something a little more direct
         //var feedChoice = Math.random(1, 3);--Buggy needs to be replaced
